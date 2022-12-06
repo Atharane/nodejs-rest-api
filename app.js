03360341300ab66d3e3b53a1296264518ea94952
NASA_Mark <76894588+Atharane@users.noreply.github.com>
2022-12-06 11:36:06 +0530

    create post request with input validation

--- app.js
+++ app.js
@@ -24,6 +24,43 @@ app.get("/api/players/:name", (req, res) => {
   res.send(player);
 });
 
+app.post("/api/players", async (req, res) => {
+  // 4oo bad request
+
+  // manual validation
+  /*
+    if (!req.body.name) {
+        res.status(400).send("Name field is mandatory");
+        return;
+    }
+    */
+
+  // Joi validation
+  const { error } = validatePlayer(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const player = {
+    name: req.body.name,
+    franchise: req.body.franchise,
+    rookieYear: req.body.rookieYear || "N/A",
+  };
+  players.push(player);
+  res.send(player);
+});
+
+// input validation
+const validatePlayer = (player) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    franchise: Joi.string().min(3).required(),
+    rookieYear: Joi.number().min(1900).max(2020).required(),
+  });
+  return schema.validate(player);
+};
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
